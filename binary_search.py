edd07271a92d68d02d3119015c10137cfb6ff38d
Rob Weber <rweber@ecec.com>
2019-03-18 14:17:09 -0500

    use logging instead of print

--- binary_search.py
+++ binary_search.py
@@ -4,7 +4,7 @@ from __future__ import division, print_function, unicode_literals
 from hashlib import sha1
 from os import stat
 import argparse
-
+import logging
 
 def binary_search(hex_hash, list_file, file_size):
     def get_full_line(file, pos):
@@ -23,7 +23,7 @@ def binary_search(hex_hash, list_file, file_size):
         #     new_pos, candidate_line.strip(), pivot))
         pwned_hash, count = candidate_line.split(':')
         if pwned_hash == my_hash:
-            print("Password found at byte {:11d}: \"{}\"".format(pivot, candidate_line.strip()))
+            logger.debug("Password found at byte {:11d}: \"{}\"".format(pivot, candidate_line.strip()))
             return int(count.strip())
         if my_hash > pwned_hash:
             return search_hash(file, my_hash, file.tell(), end)
@@ -40,11 +40,19 @@ if __name__ == "__main__":
     parser.add_argument('passwords', type=argparse.FileType('r'))
     parser.add_argument('--pwned-passwords-ordered-by-hash-filename', required=False,
                         default="pwned-passwords-sha1-ordered-by-hash-v4.txt")
+    parser.add_argument('--log',type=str,required=False,default="INFO")
     args = parser.parse_args()
+
+
+    #configure logger
+    numeric_level = getattr(logging, args.log.upper())
+    logging.basicConfig(level=numeric_level)
+    logger = logging.getLogger("pwned_offline")
+
 
     with open(args.pwned_passwords_ordered_by_hash_filename, 'r') as pwned_passwords_file:
         pwned_passwords_file_size = stat(args.pwned_passwords_ordered_by_hash_filename).st_size
-        # print("File size: {} Bytes".format(pwned_passwords_file_size))
+        logger.debug("File size: {} Bytes".format(pwned_passwords_file_size))
 
         for password in args.passwords.readlines():
             password = password.rstrip()
@@ -61,10 +69,10 @@ if __name__ == "__main__":
                     continue
             count = 0
             for h in hashes:
-                print("Searching for hash {} of password \"{}\".".format(h, password))
+                logger.info("Searching for hash {} of password \"{}\".".format(h, password))
                 count += binary_search(h, pwned_passwords_file, pwned_passwords_file_size)
             if count > 0:
-                print("Your password \"{}\" was in {} leaks or hacked databases!".format(password, count) +
+                logger.info("Your password \"{}\" was in {} leaks or hacked databases!".format(password, count) +
                       " Please change it immediately.")
             else:
-                print("Your password \"{}\" is not in the dataset. You may relax.".format(password))
+                logger.info("Your password \"{}\" is not in the dataset. You may relax.".format(password))
